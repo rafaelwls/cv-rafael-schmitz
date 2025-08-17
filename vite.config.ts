e0522f5f7e688b5ad7d923d254e4d6b607105ce6
Rafael Luan Schmitz <rafaelschmitzxd@gmail.com>
2025-08-17 19:29:08 -0300

    feat: set base path for repository in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+const REPO = "cv-rafael-schmitz";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: `/${REPO}/`,
   server: {
     host: "::",
     port: 8080,
