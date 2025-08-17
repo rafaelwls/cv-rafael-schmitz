64e08014346be3fa0dede7c29f20c0247bfc4e81
Rafael Luan Schmitz <rafaelschmitzxd@gmail.com>
2025-08-17 20:07:43 -0300

    feat: remove base path from Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: `/cv-rafael-schmitz/`,
   server: {
     host: "::",
     port: 8080,
